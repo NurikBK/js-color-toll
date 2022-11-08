bc66528bc29a61f5c17e4ebee791414a6a13bfeb
Nurzhan Bizhan <bknurik@gmail.com>
2022-11-08 19:00:48 +0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const hexInput = document.querySelector('#hexInput');
+
+
+const isValidHex = (hex) => {
+  if (!hex) return false;
+
+  const strippedHex = hex.replace('#', '');
+  return strippedHex.length === 3 || strippedHex.length === 6;
+};
+
+
