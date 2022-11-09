39826148b76c3eecebda522b7af52d146a99558c
Nurzhan Bizhan <bknurik@gmail.com>
2022-11-09 17:34:41 +0600

    add eventListener to input

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const hexInput = document.querySelector('#hexInput');
-
+const inputColor = document.querySelector('#inputColor');
 
 const isValidHex = (hex) => {
   if (!hex) return false;
 
   const strippedHex = hex.replace('#', '');
+
   return strippedHex.length === 3 || strippedHex.length === 6;
 };
+// change bgColor if input is valid
+hexInput.addEventListener('keyup', () => {
+  const hexValue = hexInput.value;
+  if (!isValidHex(hexValue)) return;
 
+  const strippedHex = hexValue.replace('#', '');
 
+  inputColor.style.backgroundColor = `#${strippedHex}`;
+});
